59d1bbd4edbd069b22ce3e472f1e5e864ca064cc
Minjae Lee <gaebok0331@gmail.com>
2021-05-13 00:30:54 +0900

    2.2 심플채팅 구현

---
+++ WebChatSolution/WebChatSolution/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebChatSolution.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            Clients.All.BroadCasting(name, message);
+        }
+    }
+}
---
+++ WebChatSolution/WebChatSolution/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+
+[assembly: OwinStartup(typeof(WebChatSolution.Startup))]
+namespace WebChatSolution
+{
+    /// <summary>
+    /// 웹 응용 프로그램이 시작되는 시점에 웹 응용 프로그램의 각종 환경 설정하는 기능을 제공
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// ASP.NET MVC5 웹 응용프로그램 환경에 ASP.NET SignalR 실시간 통신지원기능을 추가 설정
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            HubConfiguration hubConfiguration = new HubConfiguration()
+            {
+                EnableJSONP = true
+            };
+            app.MapSignalR(hubConfiguration);
+        }
+    }
+}
